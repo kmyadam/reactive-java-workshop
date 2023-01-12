208d507e20e5ffbe5d2218bcbb32dea5be3b7f4d
Kashinath <kashimca@gmail.com>
2023-01-12 21:49:19 +0530

    Exercise1 practice code

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -10,7 +11,11 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        System.out.println("Get all numbers in the ReactiveSources.intNumbersFlux stream");
+        System.out.println("into a List and print the list and its size");
+        List<Integer> integers = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println(integers);
+        System.out.println("Size : " + integers.size());
 
 
         System.out.println("Press a key to end");
--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -9,11 +10,15 @@ public class Exercise4 {
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        System.out.println("Print the value from intNumberMono when it emits");
+        ReactiveSources.intNumberMono().subscribe(number -> System.out.println(number));
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
-
+        System.out.println("Get the value from the Mono into an integer variable");
+        Integer number = ReactiveSources.intNumberMono().block();
+        System.out.println("Normal : " + number);
+        Optional<Integer> integerOptional = ReactiveSources.intNumberMono().blockOptional();
+        System.out.println("Optional : " + integerOptional.get());
         System.out.println("Press a key to end");
         System.in.read();
     }
